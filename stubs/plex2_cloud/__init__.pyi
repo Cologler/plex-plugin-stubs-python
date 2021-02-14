dbc13f6bc5da9fb8afcfabe9ededb6dfc93892b1
Cologler <skyoflw@gmail.com>
2021-02-14 15:41:56 +0800

    add cloud policy

---
+++ stubs/plex2_cloud/__init__.pyi
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2021~2999 - Cologler <skyoflw@gmail.com>
+# ----------
+#
+# ----------
+
+# class CloudPolicy(BundlePolicy, ModernPolicy)
+from plex2_bundle import *
+from plex2_modern import *
