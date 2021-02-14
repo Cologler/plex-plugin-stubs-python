70829686a143e863ae96a556c97b7a0a9e533092
Cologler <skyoflw@gmail.com>
2021-02-14 16:05:03 +0800

    add standard policy

---
+++ stubs/plex2_standard/__init__.pyi
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2021~2999 - Cologler <skyoflw@gmail.com>
+# ----------
+#
+# ----------
+
+# class StandardPolicy(BundlePolicy):
+from plex2_bundle import *
