f445541e11783f66d5aae54deb891223c61f04be
Cologler <skyoflw@gmail.com>
2021-02-14 15:42:29 +0800

    add service policy

---
+++ stubs/plex2_service/__init__.pyi
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2021~2999 - Cologler <skyoflw@gmail.com>
+# ----------
+#
+# ----------
+
+# class ServicePolicy(CodePolicy, ModernPolicy)
+from plex2_code import *
+from plex2_modern import *
+
+# ServicePolicy.environment
+__name__ = '__service__'
