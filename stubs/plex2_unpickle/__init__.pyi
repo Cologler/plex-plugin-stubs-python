f40afd4ea7f36fa73243c0ab656d1431f2cf00d2
Cologler <skyoflw@gmail.com>
2021-02-14 15:33:18 +0800

    add unpickle policy

---
+++ stubs/plex2_unpickle/__init__.pyi
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2021~2999 - Cologler <skyoflw@gmail.com>
+# ----------
+#
+# ----------
+
+# class UnpicklePolicy(BasePolicy):
+from plex2_base import *
+
+# UnpicklePolicy.environment
+from pickle import loads as unpickle
