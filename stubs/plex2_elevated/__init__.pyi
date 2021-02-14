4cfa4cceee5766b56caa151e3a2d8a109b0a0b7f
Cologler <skyoflw@gmail.com>
2021-02-14 15:40:43 +0800

    add elevated policy

---
+++ stubs/plex2_elevated/__init__.pyi
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2021~2999 - Cologler <skyoflw@gmail.com>
+# ----------
+#
+# ----------
+
+# ElevatedPolicy(BundlePolicy)
+from plex2_bundle import *
+
+# ElevatedPolicy.environment
+# hasattr/getattr/setattr/dir/super/type
