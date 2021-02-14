f64af67cfa9b04e8e9195960754cd45465290355
Cologler <skyoflw@gmail.com>
2021-02-14 15:36:44 +0800

    add model policy

---
+++ stubs/plex2_model/__init__.pyi
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2021~2999 - Cologler <skyoflw@gmail.com>
+# ----------
+#
+# ----------
+
+# class ModelPolicy(BasePolicy)
+from plex2_base import *
+
+# ModelPolicy.environment
+__name__ = '__model__'
